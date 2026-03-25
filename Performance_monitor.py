b53ddfb3cabda0bf691b944f966e4252b3994130
Li71226006 <61188965+Li71226006@users.noreply.github.com>
2026-03-25 14:51:27 +0800

    Update Performance_monitor.py

--- Performance_monitor.py
+++ Performance_monitor.py
@@ -701,7 +701,7 @@ class LLMPerformanceTester:
         from test import func1,func2,func4
 
         e = func1(a)
-        d = func2(a,b,c,d)
+        d = func2(a,b,c,d,e)
         f = func1(abc)
 
         y = func3(e)
